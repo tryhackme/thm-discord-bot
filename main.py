b85eec198d863e1540099e1d4ff8364bdfca91b6
Ubuntu <paradox@ParaBox.rmswdczp1teenhrnvubp5dq01e.bx.internal.cloudapp.net>
2019-05-16 16:41:24 +0000

    new file:   cogs/__pycache__/room.cpython-36.pyc 	new file:   cogs/__pycache__/social.cpython-36.pyc 	new file:   cogs/room.py 	new file:   cogs/social.py 	new file:   cogs/storage.json 	modified:   main.py 	modified:   setup/requirements.txt

--- main.py
+++ main.py
@@ -6,16 +6,30 @@ from discord.ext import commands
 inputFile = "token.txt"
 workingFile = open(inputFile)
 TOKEN = workingFile.readline()
-
 prefix = "!"
 bot = commands.Bot(command_prefix=prefix)
+extensions = ["cogs.room", "cogs.social"]
+if __name__ == "__main__":
+    for extension in extensions:
+        bot.load_extension(extension)
+
 
 @bot.event
 async def on_ready():
     print("Logged in as {0.user}".format(bot))
 
+
+@bot.event
+async def on_member_join(member: discord.Member):
+    channel = bot.get_channel(521382216304033794)
+    await channel.send(
+        f"<@{member.id}> welcome to the server! Be sure to review the !rules and if you have any questions fire way in #support"
+    )
+
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
+
 
 bot.run(TOKEN)
