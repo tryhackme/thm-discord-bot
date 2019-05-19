76b22c5566b077aa4c3f01c9d839320955d27a57
Robin <dubeyayushmanrobin@gmail.com>
2019-05-19 20:39:06 +0530

    Added leaderboard command

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ TOKEN = workingFile.readline()
 
 prefix = "!"
 bot = commands.Bot(command_prefix=prefix)
+initial_extensions + ["cogs.rank"]
 
 @bot.event
 async def on_ready():
@@ -17,5 +18,13 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
+
+if __name__ == "__main__":
+	for extension in initial_extensions:
+		try:
+			bot.load_extension(extension)
+			print(f"{extension} loaded successfully.\n")
+		except Exception as e:
+			print(f"Error occured while loading {extension}")
 
 bot.run(TOKEN)
