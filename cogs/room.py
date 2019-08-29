fef0cd725659d7c337f39e8f76982810ba907d6f
D4mianWayne <dubeyayushmanrobin@gail.com>
2019-08-29 18:04:28 +0530

    Temporary fix

--- cogs/room.py
+++ cogs/room.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-import discord, random, time, asyncio, requests, json
+import discord, random, time, asyncio, aiohttp, json
 
 
 class Room(commands.Cog):
@@ -11,23 +11,24 @@ class Room(commands.Cog):
             channel = self.bot.get_channel(546650767495397376)
             json_file = open("storage.json", "r+")
             stored_data = json.load(json_file)
-            new_data = requests.get("https://www.tryhackme.com/api/newrooms")
-            text = new_data.text
-            json_data = new_data.json()
-            # check for new data
-            if json_data[0]["title"] != stored_data[0]["title"]:
-                # set up embed
-                img = "https://tryhackme.com/" + json_data[0]["image"]
-                title = json_data[0]["title"]
-                code = "https://tryhackme.com/room/" + json_data[0]["code"]
-                description = json_data[0]["description"]
-                embed = discord.Embed(title=title, description=description, url=code)
-                embed.set_image(url=img)
-                await channel.send("NEW ROOM")
-                await channel.send(embed=embed)
-                with open("storage.json", "w") as file:
-                    file.write(text)
-                    file.close()
+            async with aiohttp.ClientSession() as session:
+                async with session.get("https://tryhackme.com/api/newrooms") as new_data:
+                    text = await new_data.read()
+                    json_data = json.loads(text)
+                    # check for new data
+                    if json_data[0]["title"] != stored_data[0]["title"]:
+                        # set up embed
+                        img = "http://tryhackme.com/" + json_data[0]["image"]
+                        title = json_data[0]["title"]
+                        code = "http://tryhackme.com/room/" + json_data[0]["code"]
+                        description = json_data[0]["description"]
+                        embed = discord.Embed(title=title, description=description, url=code)
+                        embed.set_image(url=img)
+                        await channel.send("NEW ROOM")
+                        await channel.send(embed=embed)
+                        with open("storage.json", "w") as file:
+                            file.write(text)
+                            file.close()
             await asyncio.sleep(60)
 
     @commands.Cog.listener()
