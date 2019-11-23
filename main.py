2f12fb2b5bf477d173666357048f4c06e1632387
Dan (SherlockSec) <dan@sherlock-security.com>
2019-11-23 22:48:35 +0000

    Tweaked the welcome message to fit current channels.

--- main.py
+++ main.py
@@ -3,12 +3,43 @@ from discord.ext import commands
 import time
 import datetime
 
+import random
+
 START_TIME = time.time()
 
 inputFile = "token.txt"
 workingFile = open(inputFile)
 TOKEN = workingFile.readline()
 
+quotes = [
+    "C4n y0u pwn th4 m4chin3?",
+    "Hacker man 0x1 0x0 0x1",
+    "The quieter you become the more you are able to hear",
+    "\*Morpheus\*: Red or Blue pill?",
+    "Access security... Access security grid... YOU DIDN'T SAY THE MAGIC WORD!",
+    "Just hack the mainframe.",
+    "Z2VsdW5weHpyLnBieg==",
+    "The Matrix is real",
+    "No place like 127.0.0.1",
+    "Hack the planet",
+    "Just obfuscate it...",
+    "Armitage + Hail Mary",
+    "WEP, WPA, WAH?",
+    "admin:password",
+    "rockyou.txt",
+    "tmux > screens",
+    "tabs or spaces?",
+    "Leeerrrroy Jeekinnnns...",
+    "Enumeration is key",
+    "Try harder..",
+    "https://discord.gg/zGdzUad",
+    "Satoshi Nakamoto",
+    "Mining Bitcoin...",
+    "Configuring neural network"
+    ]
+
+def getMoto():
+    return quotes[random.randint(0, len(quotes) - 1)]
 
 prefix = "!"
 bot = commands.Bot(command_prefix=prefix)
@@ -31,9 +62,11 @@ async def on_ready():
 @bot.event
 async def on_member_join(member: discord.Member):
     channel = bot.get_channel(521382216304033794)
-    await channel.send(
-        f"<@{member.id}>Welcome to the server!\nBe sure to review the **!rules** and if you have any questions fire way in #support"
-    )
+    response = discord.Embed(title="Welcome!", description=quip, color=0xa20606)
+    response.set_author(name="TryHackMe",icon_url="https://tryhackme.com/img/THMlogo.png")
+    response.set_thumbnail(url="https://cdn.discordapp.com/icons/521382216299839518/c0c7e9f1e258dd6d030fde8823bf8657.webp")
+    response.add_field(name="Hey there!", value=f"<@{member.id}> , Welcome to the server!\n Be sure to review the !rules in #bot-commands. If you need help with a room, ask in #rooms-help.")
+    await ctx.send(embed=response)
 
 @bot.command()
 async def uptime(self, ctx):
