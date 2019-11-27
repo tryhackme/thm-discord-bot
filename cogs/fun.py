be9623fd529c3fdff9fcfdde66063f09eda46dc1
horshark <mail@horshark.me>
2019-11-27 15:11:00 -0800

    +Fixed the welcome message, rules are now sent in DM and the welcoming message itself is fixed.; +Added dark, skidy and ashu's quotes fun commands.

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,9 +1,34 @@
 from discord.ext import commands
 import discord
+import json
+import random
+
 
 class Fun(commands.Cog, name="Fun Commands"):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def skidy(self, ctx):
+        response = discord.Embed(title=":slight_smile:", description="", color=0x225999)
+        response.set_author(name="Skidy",icon_url="https://i.imgur.com/fSMnXPt.png")
+        await ctx.send(embed=response)
+
+    @commands.command()
+    async def ashu(self, ctx):
+        response = discord.Embed(title=":slight_smile:", description="", color=0x225999)
+        response.set_author(name="Ashu",icon_url="https://i.imgur.com/ojiqdem.png")
+        await ctx.send(embed=response)
+
+    @commands.command()
+    async def dark(self, ctx):
+        darkF = json.loads(open("config/dark.json", "r").read())
+        darkQuotes = darkF["quotes"]
+
+        response = discord.Embed(title=darkQuotes[random.randint(0, len(darkQuotes)-1)], description="", color=0xff4500)
+        response.set_author(name="DarkStar7471",icon_url="https://i.imgur.com/jZ908d1.png")
+        await ctx.send(embed=response)
+
 
     @commands.command()
     async def honk(self, ctx):
@@ -14,9 +39,6 @@ class Fun(commands.Cog, name="Fun Commands"):
     
     @commands.command()
     async def boop(self, ctx, member: discord.Member=None):
-        #print(member.name)
-        #print(member.id)
-        #print(ctx.message.channel.name)
         if ctx.message.channel.name == "bot-commands":
                 if member.name == "Asphodel": #Yume - Asphodel#8097
                     print("No boop")
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 import time
 import datetime
 import json
-
 import random
 
 
@@ -40,6 +39,23 @@ def getMoto():
     else:
         return regularQuotes[random.randint(0, len(regularQuotes) - 1)]
 
+async def send_rules(member):
+    response = discord.Embed(title="!rules", color=0xffff00)
+    response.set_author(name="TryHackMe",icon_url="https://tryhackme.com/img/THMlogo.png")
+    response.set_thumbnail(url="https://tryhackme.com/img/THMlogo.png")
+            
+    response.add_field(name="1.", value="No unsolicited direct messages (DMs) to other members of the discord. This includes staff. Verify that the member you are messaging is ok with you sending them DMs.")
+    response.add_field(name="2.", value="No personal drama or drama from any other discord community is allowed to be brought into this discord. This is a space for infosec discussions and learning, keep it that way.")
+    response.add_field(name="3.", value="No excessive self promotion. Linking to another discord server is strictly prohibited, just don't turn it into advertising.")
+    response.add_field(name="4.", value="Keep it civil. If action is necessary in a dispute or any other sort of disruption on this discord punishment will be doled out evenly both to the individual(s) who started the issue and to those who reacted inappropriately in their response.")
+    response.add_field(name="5.", value="No cheating is allowed whatsoever within this discord. Any cheating (other than specifically within a developmental environment where it has been preapproved by staff) will result in an immediate and permanent ban.")
+    response.add_field(name="6.", value="Racism is not tolerated and will result in a permanent ban.")
+    response.add_field(name="7.", value="Administrators reserve the right to modify the rules at any time and extend them accordingly to cover infractions which may not be currently included in these rules.")
+    response.set_footer(text="From the TryHackMe Official API!")
+    
+    channel = await member.create_dm()
+    await channel.send(embed=response)
+
 # Loading the cogs.
 if __name__ == "__main__":
     for extension in extensions:
@@ -59,12 +75,14 @@ async def on_ready():
 @bot.event
 async def on_member_join(member: discord.Member):
     channel = bot.get_channel(welcomeChanID)
-    time.sleep(5)
+
     quip = getMoto()
     response = discord.Embed(title="Welcome!", description=quip, color=0xa20606)
     response.set_author(name="TryHackMe",icon_url="https://tryhackme.com/img/THMlogo.png")
     response.set_thumbnail(url="https://cdn.discordapp.com/icons/521382216299839518/c0c7e9f1e258dd6d030fde8823bf8657.webp")
-    response.add_field(name="Hey there!", value=f"<@{member.id}> , Welcome to the server!\n Be sure to review the !rules in #bot-commands. If you need help with a room, ask in #rooms-help.\n\n You can also sync your THM rank on the discord! Use !verify in #bot-commands for more information!")
+    response.add_field(name="Hey there!", value=member.mention + ", Welcome to the server!\nIf you need help with a room, ask in #rooms-help.\n\n You can also sync your THM rank on the discord! Use !verify in #bot-commands for more information!")
+
+    await send_rules(member)
     await channel.send(embed=response)
 
 
