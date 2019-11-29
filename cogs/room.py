267bab707589d062ec75db79f3ef7ee12ea81a71
horshark <mail@horshark.me>
2019-11-29 15:46:43 -0800

    +Fixed the room.py COG, fixed the picture URL that was wrong, changed to a better message, etc.

--- cogs/room.py
+++ cogs/room.py
@@ -8,29 +8,41 @@ class Room(commands.Cog):
 
     async def new_room(self):
         while True:
-            channel = self.bot.get_channel(546650767495397376)
-            json_file = open("config/storage.json", "r", encoding="utf-16").read()
-            stored_data = json.loads(json_file)
+            channelJson = open("config/channels.json", "r").read()
+            channelID = json.loads(channelJson)["announcements"]
+            channel = self.bot.get_channel(channelID)
+
+            roomJson = open("config/storage.json", "r").read()
+            stored_data = json.loads(roomJson)
+
             async with aiohttp.ClientSession() as session:
                 async with session.get("https://tryhackme.com/api/newrooms") as new_data:
+
                     text = await new_data.read()
                     json_data = json.loads(text)
+
                     # check for new data
                     if json_data[0]["title"] != stored_data[0]["title"]:
+                       
                         # set up embed
-                        img = "http://tryhackme.com/" + json_data[0]["image"]
+                        img = json_data[0]["image"]
                         title = json_data[0]["title"]
                         code = "http://tryhackme.com/room/" + json_data[0]["code"]
                         description = json_data[0]["description"]
+
                         embed = discord.Embed(title=title, description=description, url=code)
                         embed.set_image(url=img)
                         embed.set_author(name="TryHackMe",icon_url="https://tryhackme.com/img/THMlogo.png")
                         embed.set_footer(text="From the TryHackMe Official API!")
-                        await channel.send("NEW ROOM")
+
+                        await channel.send("A new room is available!  |  Check it out: https://tryhackme.com/room/"+code)
                         await channel.send(embed=embed)
+
                         with open("config/storage.json", "w") as file:
-                            file.write(text)
-                            file.close()
+                            json.dump(json_data, file)
+                            #file.write(str(json_data))
+                            #file.close()
+                            
             await asyncio.sleep(60)
 
     @commands.Cog.listener()
