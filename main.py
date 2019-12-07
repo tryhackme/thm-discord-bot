088befe4a6dd1afa254da726cd03a5ce89baa34b
horshark <mail@horshark.me>
2019-12-07 10:47:36 -0800

    ~Made the welcome message clearer.

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ async def on_member_join(member: discord.Member):
     # Embed creation.
     response.set_author(name="TryHackMe",icon_url="https://tryhackme.com/img/THMlogo.png")
     response.set_thumbnail(url="https://cdn.discordapp.com/icons/521382216299839518/c0c7e9f1e258dd6d030fde8823bf8657.webp")
-    response.add_field(name="Hey there!", value=member.mention + ", Welcome to the server!\nIf you need help with a room, ask in #rooms-help.\n\n You can also sync your THM rank on the discord! Use !verify in #bot-commands for more information!")
+    response.add_field(name="Hey there!", value=member.mention + ", Welcome to the server!\nIf you need help with a room, ask in #rooms-help.\n\n You can also sync your THM rank on the discord, check your DMs!")
     
     # Check if user exists. (avoids join-leavers etc.)
     if member is not None:
