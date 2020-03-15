8e5ce75985caf06ce5f68c1993320bc01809a046
horshark <accounts@horshark.me>
2020-03-15 15:55:08 +0100

    +Writeup command; +Multivpn command; +Newroom command

--- cogs/room.py
+++ cogs/room.py
@@ -12,6 +12,10 @@ channelID = json.loads(channelJson)["announcements"]
 rolesF = json.loads(open("config/roles.json", "r").read())
 adminID = rolesF["admin"]
 
+# API
+api_url = "https://tryhackme.com/api/"
+
+
 # Role managment function.
 def hasRole(member, id):
         for role in member.roles:
@@ -20,12 +24,15 @@ def hasRole(member, id):
         return False
 
 # Sending announcement function.
-async def send(channel, json_data):
+async def send(channel, json_data, code=None):
     # Set up embed.
-    img = json_data[0]["image"]
-    title = json_data[0]["title"]
-    code = "http://tryhackme.com/room/" + json_data[0]["code"]
-    description = json_data[0]["description"]
+    img = json_data["image"]
+    title = json_data["title"]
+    if code==None:
+        code = "http://tryhackme.com/room/" + str(json_data["code"])
+    else:
+        code = "http://tryhackme.com/room/" + code
+    description = json_data["description"]
 
     embed = officialEmbed(title, description)
     embed.set_image(url=img)
@@ -48,16 +55,63 @@ class Room(commands.Cog):
         response.add_field(name="Learn how to use OpenVPN to connect to our network!", value="https://tryhackme.com/room/openvpn")
         await ctx.send(embed=response)
 
+    @commands.command(description="Learn how to look for duplicate instance of your OpenVPN connection.")
+    async def multivpn(self, ctx):
+        response = officialEmbed()
+        response.set_thumbnail(url="https://tryhackme.com/room/openvpn")
+        response.add_field(name="• Step 1", value="Type ps aux | grep openvpn into your terminal and press enter")
+        response.add_field(name="• Step 2", value="If there's more than one line (and the second doesn't have \"grep\" in it), do the following steps")
+        response.add_field(name="• Step 3", value="Type killall openvpn into your terminal and press enter")
+        response.add_field(name="• Step 4", value="Start the VPN with sudo openvpn <path-to-config>")
+        await ctx.send(embed=response)
+
+    @commands.command(description="Get the writeups for a room.", usage="{room_code}")
+    async def writeup(self, ctx, room_code):
+        # Request to the API.
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url+"room/"+room_code) as new_data:
+                text = await new_data.read()
+                json_data = json.loads(text)
+
+                # If the specified code is wrong.
+                if json_data["success"] == False:
+                    botMsg = await ctx.send("Sorry, but the room code ``"+room_code+"`` could not be found.")
+
+                    time.sleep(5)
+                    await botMsg.delete()
+                    await ctx.message.delete()
+
+                    return
+
+                # If there is no writeup.
+                if len(json_data["writeups"]) == 0:
+                    await ctx.send("Sorry, there is no writeup for this room.")
+                    return
+
+                # Set up embed.
+                img = json_data["image"]
+                title = json_data["title"]
+                link = "http://tryhackme.com/room/" + room_code
+
+                embed = officialEmbed(title, link)
+                embed.set_image(url=img)
+
+                for wu in json_data["writeups"]:
+                    embed.add_field(name="By: "+wu["username"], value=wu["link"])
+
+                # Send messages.
+                await ctx.send(embed=embed)     
+
     @commands.command(description="Manually announce the last room.", hidden=True)
     async def room(self, ctx):
-        if not hasRole(ctx.author, adminID):
-            botMsg = await ctx.send("You do not have the permission to do that.")
+        # if not hasRole(ctx.author, adminID):
+        #     botMsg = await ctx.send("You do not have the permission to do that.")
             
-            time.sleep(5)
+        #     time.sleep(5)
 
-            await botMsg.delete()
-            await ctx.message.delete()
-            return
+        #     await botMsg.delete()
+        #     await ctx.message.delete()
+        #     return
         
         # Gets channel.
         channel = self.bot.get_channel(channelID)
@@ -68,9 +122,26 @@ class Room(commands.Cog):
                 text = await new_data.read()
                 json_data = json.loads(text) 
         
-        await send(channel, json_data)
+        await send(channel, json_data[0])
 
-    async def new_room(self):
+    @commands.command(name="newroom", description="Manually announce a new room.", usage="{room_code}", hidden=True)
+    async def new_room(self, ctx, room):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url+"room/"+room) as new_data:
+                text = await new_data.read()
+                json_data = json.loads(text)
+
+                if json_data["success"] == False:
+                    await ctx.send("Sorry, but the room code ``"+room+"`` could not be found.")
+                    return
+
+                channelJson = open("config/channels.json", "r").read()
+                channelID = json.loads(channelJson)["announcements"]
+                channel = self.bot.get_channel(channelID)
+
+                await send(channel, json_data, room)
+
+    async def new_room_listener(self):
         # Setup the channel to send the announcements into.
         channelJson = open("config/channels.json", "r").read()
         channelID = json.loads(channelJson)["announcements"]
@@ -84,7 +155,7 @@ class Room(commands.Cog):
 
             # Getting infos from the API.
             async with aiohttp.ClientSession() as session:
-                async with session.get("http://tryhackme.com/api/newrooms") as new_data:
+                async with session.get(api_url+"newrooms") as new_data:
 
                     text = await new_data.read()
                     json_data = json.loads(text)
@@ -93,7 +164,7 @@ class Room(commands.Cog):
                     # Try-except to avoid wrongly [...] (making the bot more stable thx to this)
                     try:
                         titleJsonData = json_data[0]["title"]
-                        titleStoredData = stored_data[0]["title"]
+                        titleStoredData = stored_data["title"]
                     except:
                         copyfile("config/room_default.json", "config/room.json")
                         
@@ -105,17 +176,14 @@ class Room(commands.Cog):
 
                     # Check for new data.
                     if titleJsonData != titleStoredData:
-                        await send(channel, json_data)
+                        await send(channel, json_data[0])
                             
             await asyncio.sleep(60)
 
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.new_room()
+        await self.new_room_listener()
 
 
 def setup(bot):
     bot.add_cog(Room(bot))
-
-
-
