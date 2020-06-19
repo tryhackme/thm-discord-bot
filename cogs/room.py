194f6cdc4442c4e3d763f534be01afadf07d4ad4
horshark <accounts@horshark.me>
2020-06-19 18:58:44 +0200

    Fixed room link
    The link was to the API instead of actual room

--- cogs/room.py
+++ cogs/room.py
@@ -56,9 +56,9 @@ async def announce_room(channel, json_data, code=None):
     img = json_data["image"]
     title = json_data["title"]
     if code == None:
-        url = c_api_url["room"] + str(json_data["code"])
+        url = c_url_room["room"] + str(json_data["code"])
     else:
-        url = c_api_url["room"] + code
+        url = c_url_room["room"] + code
     description = json_data["description"]
 
     embed = officialEmbed(title, description)
