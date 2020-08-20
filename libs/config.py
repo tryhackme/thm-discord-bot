a8d461de3cf01369a8bf06695b299b45c613bc3f
smackhack <48143394+smackhack@users.noreply.github.com>
2020-08-20 00:11:00 -0700

    Fixed file i/o
    Properly reads files with context manager
    Cleaned up variable names for consistency

--- libs/config.py
+++ libs/config.py
@@ -1,15 +1,18 @@
 import json
 
-configfile = "config/config.json"
-stringsFile = "config/strings.json"
+config_path = "config/config.json"
+strings_path = "config/strings.json"
 
-jsonConfig = json.loads(open(configfile, "r").read())
-jsonStrings = json.loads(open(stringsFile, "r").read())
+with open(config_path) as config_file, \
+     open(strings_path) as strings_file:
+
+    config_json = json.loads(config_file.read())
+    strings_json = json.loads(strings_file.read())
 
 
 def get_config(key):
-    return jsonConfig[key]
+    return config_json[key]
 
 
 def get_string(key):
-    return jsonStrings[key]
+    return strings_json[key]
