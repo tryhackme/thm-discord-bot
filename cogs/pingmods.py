7a3aa5e5f5a848461514c8432df1dc99504a2acb
Ben (CMNatic) <ben@cmnatic.co.uk>
2020-09-10 11:13:04 +0100

    Updated usage and re-used CM "channel" check

--- cogs/pingmods.py
+++ cogs/pingmods.py
@@ -22,30 +22,22 @@ id_mod = config.get_config("roles")["mod"]
 s_pingmods = config.get_string("ping_mods")
 
 
-# Load error message used for stating that the channel the message originates from is wrong
-s_channel_error = config.get_string("commands")["channel_not_allowed"]
-
 class PingMods(commands.Cog, name="Ping Mods"):
     def __init__(self, bot):
         self.bot = bot
     
     # Checks to see if the user is a Mentor and if so, will only send in #staff_lounge
-    # I have left "channels" blank here because the Command Manager (CM) does not (yet) have a way of differentiating what part of the check has failed
+    # Command Manager (CM) does not (yet) have a way of differentiating what part of the check has failed
     # I.e. All conditions in the check must be true for command to work, however any command that is wrong the check will just return false
     # This means that the Command Manager will return the error_response it's been coded to return, where you cannot modify the behaviour of it outside of the CM
     # In this case I wanted the error messages given by the bot to persist in chat.
-    @commands.command(name="pingmods", description=s_pingmods["help_desc"], usage="!pingmods 'reason'", hidden=True)
+    @commands.command(name="pingmods", description=s_pingmods["help_desc"], usage="!pingmods {reason}", hidden=True)
     @check(roles="mentor",
-        channels="",
+        channels="staff_lounge",
         dm_flag=False)
 
 
     async def ping_mods(self, ctx, *reason):
-
-        # If the channel the message is being sent from is not lounge, send "command not allowed in channel" but don't delete
-        if ctx.channel.id != id_staff_lounge:
-            await command_manager.error_response(ctx, s_channel_error, delete_msg=False, delete_ctx=False)
-            return
 
         # If no reason is provided, send "Please provide a reason" but do not delete
         if not reason:
